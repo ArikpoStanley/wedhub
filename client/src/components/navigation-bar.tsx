60f844ddb92cb1ce86fa3cd99a0f8a14fd8752a8
David Ezekiel <titusdavid39@gmail.com>
2025-08-18 04:55:37 +0100

    logo

--- client/src/components/navigation-bar.tsx
+++ client/src/components/navigation-bar.tsx
@@ -96,7 +96,7 @@ export default function NavigationBar({ currentPage, showBackButton = false }: N
                 <img
                   src="https://res.cloudinary.com/dycukxm7r/image/upload/v1755525560/EBLogo-removebg-preview_cskhkd.png"
                   alt="E&B Wedding Logo"
-                  className="w-16 h-16 object-contain cursor-pointer hover:scale-105 transition-transform"
+                  className="w-16 h-16 object-contain cursor-pointer hover:scale-105 hover:rotate-360 transition-transform duration-700"
                   onClick={handleLogoClick}
                 />
               </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,6 +80,9 @@ export default {
           },
         },
       },
+      rotate: {
+        '360': '360deg',
+      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
